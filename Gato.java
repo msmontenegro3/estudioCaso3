60f947433fa98773d704bc8b5e980656716aa3fb
Mirta Montenegro <mirta.s.montenegro.l@gmail.com>
2022-06-08 22:24:42 -0500

    comentarios actualizados

--- Gato.java
+++ Gato.java
@@ -3,14 +3,17 @@ package EstudioCaso3;
 
 public class Gato extends Animal{
 	
+	//CONSTRUCTOR
 	public Gato(String nombre, String raza){
 		super(nombre, raza);
-	}
+	}//fin constructor
 	
+	//MÉTODOS PROPIOS
 	public String diOnomatopeya() {
 		String sonidoGato=super.diOnomatopeya();
 		return sonidoGato + "Miau";
-	}
+	}//fin método diOnomatopeya
+	
 	//Método toString
 	public String toString() {
 		return String.format("Nombre: %s \nRaza: %s \n %s:",getNombre(), getRaza(), diOnomatopeya());
--- Perro.java
+++ Perro.java
@@ -2,14 +2,17 @@
 package EstudioCaso3;
 
 public class Perro extends Animal{
+	
+	//CONSTRUCTOR
 	public Perro(String nombre, String raza){
 		super(nombre, raza);
-	}
+	}//fin constructor
 	
+	//MÉTODOS PROPIOS
 	public String diOnomatopeya() {
 		String sonidoPerro = super.diOnomatopeya();
 		return sonidoPerro + "Guau";
-	}
+	}//fin método diOnomatopeya
 	//Método toString
 		public String toString() {
 			return String.format("Nombre: %s \nRaza: %s \n %s:",getNombre(), getRaza(), diOnomatopeya());
